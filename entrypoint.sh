debe85cc5c921c8d7494f0cef852266fe1fae498
Mohit Sinha <mohitsinha.it@gmail.com>
2018-12-21 13:10:32 +0700

    Keystore changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-keytool -genkey -noprompt -storepass password -keypass password -keyalg RSA -alias tomcat -dname "CN=tomcat" -keystore /usr/share/tomcat.keystore
-
+keytool -genkey -noprompt -storepass password -keypass password -keyalg RSA -alias tomcat  -keystore /usr/local/tomcat.keystore -dname "CN=test, OU=ID, O=test, L=test, ST=test, C=ID"
 catalina.sh run
 
